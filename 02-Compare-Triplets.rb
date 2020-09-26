4917d06597eda24d7479c612d96efa1405a4d149
Dani <daniaxasabraxas@gmail.com>
2020-09-26 17:49:32 -0300

    Trying to solve the Sales by Match Problem

--- 02-Compare-Triplets.rb
+++ 02-Compare-Triplets.rb
@@ -2,6 +2,7 @@ def compare(a, b)
   count = 0
   points_a = 0
   points_b = 0
+  ar_result = []
   while count != a.length()
     #puts a[count].to_s + " " + b[count].to_s
     if a[count] > b[count]
@@ -11,8 +12,11 @@ def compare(a, b)
     end
     count += 1
   end
-  puts points_a.to_s + " " + points_b.to_s
+  ar_result.push(points_a)
+  ar_result.push(points_b)
+  return ar_result
+  #puts points_a.to_s + " " + points_b.to_s
   #puts a.length().to_s + " " + count.to_s
 end
 
-puts compare([1, 2, 3], [3, 2, 1])
+puts compare [1, 2, 3], [3, 2, 1]
